3e8dcdc8193e82ebb949353e4a1e99b3d5f15916
lenazh <lena.zhivun@berkeley.edu>
2015-12-23 15:07:21 -0800

    Connect to Google Drive

--- DogeStation2/Form1.cs
+++ DogeStation2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GDriveWriter;
 
 namespace DogeStation2
 {
@@ -15,6 +16,19 @@ namespace DogeStation2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var writer = new GDriveWriter.GDriveWriter("nuri-station", "nuri-station.json");
+                writer.ListFiles();
+            }
+            catch (Exception exception)
+            {
+                // Do nothing.
+            }
         }
     }
 }
