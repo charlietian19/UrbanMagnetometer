884badb258434dbca722c70abfcbc59ea01f405e
lenazh <lena.zhivun@berkeley.edu>
2016-01-02 18:10:47 -0800

    Refactoring

--- UnitTests/GDrive/GDrivePathHelperTest.cs
+++ UnitTests/GDrive/GDrivePathHelperTest.cs
@@ -11,8 +11,10 @@ namespace UnitTests
     [TestClass]
     public class GDrivePathHelperTest
     {
-        private Mock<File> rootMock;
-        private File root;
+        private Mock<File> rootMock, fooMock, barMock;
+        private File root, bar, foo;
+        private Mock<ChildReference> fooChild, barChild;
+        private Mock<IGDrive> google;
 
         /* Returns a file mock given title. */
         private Mock<File> SetupMockFile(string title)
@@ -24,18 +26,30 @@ namespace UnitTests
         }
 
         [TestInitialize]
+        /* Setup root, foo, and bar folders. */
         public void TestSetup()
         {
             rootMock = SetupMockFile("Root");
             root = rootMock.Object;
+            fooMock = SetupMockFile("foo");
+            foo = fooMock.Object;
+            barMock = SetupMockFile("bar");
+            bar = barMock.Object;
+            fooChild = new Mock<ChildReference>();
+            barChild = new Mock<ChildReference>();
+            barChild.Setup(o => o.Id).Returns(bar.Id);
+            fooChild.Setup(o => o.Id).Returns(foo.Id);
+
+            google = new Mock<IGDrive>();
+            google.Setup(o => o.GetRootFolderId()).Returns(root.Id);
+            google.Setup(o => o.GetFileInfo(root.Id)).Returns(root);
+            google.Setup(o => o.GetFileInfo(foo.Id)).Returns(foo);
+            google.Setup(o => o.GetFileInfo(bar.Id)).Returns(bar);
         }
 
         [TestMethod]
         public void LookupPathRoot()
         {
-            var google = new Mock<IGDrive>();
-            google.Setup(o => o.GetRootFolderId()).Returns(root.Id);
-            google.Setup(o => o.GetFileInfo(root.Id)).Returns(root);
             IGDrivePathHelper path = new GDrivePathHelper(google.Object);
             Assert.AreEqual(root.Title, path.PathToGoogleFile(@"\").Title);
         }
@@ -43,25 +57,18 @@ namespace UnitTests
         [TestMethod]
         public void LookupFileInRootDirectory()
         {
-            var fooMock = SetupMockFile("foo");
-            var foo = fooMock.Object;
-            var barMock = SetupMockFile("bar");
-            var bar = barMock.Object;
-            var google = new Mock<IGDrive>();
-            var fooChild = new Mock<ChildReference>();
-            var barChild = new Mock<ChildReference>();
-            barChild.Setup(o => o.Id).Returns(bar.Id);
-            fooChild.Setup(o => o.Id).Returns(foo.Id);
-            google.Setup(o => o.GetRootFolderId()).Returns(root.Id);
-            google.Setup(o => o.GetFileInfo(root.Id)).Returns(root);
-            google.Setup(o => o.GetFileInfo(foo.Id)).Returns(foo);
-            google.Setup(o => o.GetFileInfo(bar.Id)).Returns(bar);
             google.Setup(o => o.ChildList(root)).Returns(
                 new ChildReference[] {barChild.Object, fooChild.Object});
             IGDrivePathHelper path = new GDrivePathHelper(google.Object);
 
             Assert.AreEqual(foo.Title, path.PathToGoogleFile(@"\foo").Title);
             Assert.AreEqual(bar.Title, path.PathToGoogleFile(@"\bar").Title);
+        }
+
+        [TestMethod]
+        public void LookupFilesInFolder()
+        {
+
         }
     }
 }
