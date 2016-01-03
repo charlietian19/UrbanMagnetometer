747ce41ba9381380d0eeee13921b42648a54e914
lenazh <lena.zhivun@berkeley.edu>
2016-01-02 17:31:51 -0800

    Added unit tests for GDrivePathHelper

---
+++ UnitTests/GDrive/GDrivePathHelperTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Google.Apis.Drive.v2.Data;
+using GDriveNURI;
+using Moq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class GDrivePathHelperTest
+    {
+        [TestMethod]
+        public void LookupPathRoot()
+        {
+            var google = new Mock<IGDrive>();
+            google.Setup(o => o.GetRootFolderId()).Returns("rootID");
+            google.Setup(o => o.GetFileInfo("rootID"))
+                .Returns(new File() { Title = "Root", Id = "rootID" });
+            IGDrivePathHelper path = new GDrivePathHelper(google.Object);
+            Assert.AreEqual("Root", path.PathToGoogleFile(@"\").Title);
+            Assert.AreEqual("rootID", path.PathToGoogleFile(@"\").Id);
+        }
+    }
+}
